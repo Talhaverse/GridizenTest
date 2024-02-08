64dbeff1ed7e320a9acea9b4891806281d2054d2
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-02-08 15:51:49 +0500

    redux practice

--- App.tsx
+++ App.tsx
@@ -1,44 +1,57 @@
-import { View, Text,SafeAreaView,StyleSheet } from 'react-native'
-import React from 'react'
-import Product from './screens/Product'
-import Header from './screens/Header'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Button,
+} from 'react-native';
+import React from 'react';
+import Product from './screens/Product';
+import Header from './screens/Header';
 
 const App = () => {
-
-const products = [
-  {
-    name: 'Samsung Mobile',
-    color: 'white',
-    price: 3000,
-    image : 'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8'
-  },
-  {
-    name: 'Apple Mobile',
-    color: 'white',
-    price: 6000,
-    image : 'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8'
-  },
-  {
-    name: 'Oppo Mobile',
-    color: 'white',
-    price: 1000,
-    image : 'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8'
-  },
-]
-
+  const products = [
+    {
+      name: 'Samsung Mobile',
+      color: 'white',
+      price: 3000,
+      image:
+        'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
+    },
+    {
+      name: 'Apple Mobile',
+      color: 'blue',
+      price: 6000,
+      image:
+        'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
+    },
+    {
+      name: 'Oppo Mobile',
+      color: 'green',
+      price: 1000,
+      image:
+        'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
+    },
+  ];
 
   return (
-    <View style={styles.container} >
+    <SafeAreaView style={styles.container}>
       <Header />
-      <Product />
-    </View>
-  )
-}
+      <ScrollView>
+        {
+        products.map((item) => <Product item={item} />)
+        }
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex:1
-  }
-})
+    flex: 1,
+  },
+});
 
-export default App
+export default App;
--- screens/Product.js
+++ screens/Product.js
@@ -1,10 +1,23 @@
-import { View, Text, SafeAreaView,StyleSheet } from 'react-native'
+import { View, Text, SafeAreaView,StyleSheet,Image,Button } from 'react-native'
 import React from 'react'
 
-const Product = () => {
+const Product = (props) => {
+
+  const item = props.item
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Products</Text>
+
+<View style={{}}>
+            <Text style={{fontSize: 24}}>{item.name}</Text>
+            <Text style={{fontSize: 24}}>{item.price}</Text>
+            <Text style={{fontSize: 24}}>{item.color}</Text>
+            <Image
+              style={{width: 100, height: 100}}
+              source={{uri: item.image}}
+            />
+            <Button title="Add to Cart" />
+          </View>
     </SafeAreaView>
   )
 }

46aab4cb47b087b44b8aeec2ca738b60c422913d
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-02-17 15:53:26 +0500

    adding nabigation with redux

--- App.tsx
+++ App.tsx
@@ -8,57 +8,21 @@ import {
   Button,
 } from 'react-native';
 import React from 'react';
-import Product from './screens/Product';
-import Header from './screens/Header';
+// import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ProductWrapper from './screens/ProductWrapper';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const products = [
-    {
-      name: 'Samsung Mobile',
-      color: 'white',
-      price: 3000,
-      image:
-        'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
-    },
-    {
-      name: 'Apple Mobile',
-      color: 'blue',
-      price: 6000,
-      image:
-        'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
-    },
-    {
-      name: 'Oppo Mobile',
-      color: 'green',
-      price: 1000,
-      image:
-        'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
-    },
-    {
-      name: 'Blackberry Mobile',
-      color: 'black',
-      price: 10000,
-      image:
-        'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
-    },
-  ];
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <ScrollView>
-        {
-        products.map((item) => <Product item={item} />)
-        }
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Product" component={ProductWrapper} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
---
+++ screens/ProductWrapper.js
@@ -0,0 +1,65 @@
+import {
+    View,
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    Image,
+    ScrollView,
+    Button,
+  } from 'react-native';
+  import React from 'react';
+  import Product from './Product';
+  import Header from './Header';
+  
+  const ProductWrapper = () => {
+    const products = [
+      {
+        name: 'Samsung Mobile',
+        color: 'white',
+        price: 3000,
+        image:
+          'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
+      },
+      {
+        name: 'Apple Mobile',
+        color: 'blue',
+        price: 6000,
+        image:
+          'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
+      },
+      {
+        name: 'Oppo Mobile',
+        color: 'green',
+        price: 1000,
+        image:
+          'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
+      },
+      {
+        name: 'Blackberry Mobile',
+        color: 'black',
+        price: 10000,
+        image:
+          'https://fastly.picsum.photos/id/365/200/200.jpg?hmac=1d3GDxGN6ctXX3y8q4PA_hKu6fLOCEGbgeKZKJ8K8U8',
+      },
+    ];
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <ScrollView>
+          { 
+          products.map((item) => <Product item={item} />)
+          }
+        </ScrollView>
+      </SafeAreaView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
+  
+  export default ProductWrapper;
+  

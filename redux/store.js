1196223ae2ac5325b9b8c9c19b5dbc6ad039b41c
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-02-17 19:28:04 +0500

    redux saga impl

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import rootReducer from './rootReducer'
+import createSagaMiddleware from 'redux-saga';
+import SagaData from './Saga';
+const sagaMiddleware = createSagaMiddleware()
 
 const store = configureStore({
-    reducer:rootReducer
+    reducer:rootReducer,
+    middleware:()=>[sagaMiddleware]
 });
 
+sagaMiddleware.run(SagaData)
 export default store;
